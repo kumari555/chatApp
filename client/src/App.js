809e58157ec42e3cb6e05e1f9d521e397af293b2
kumari555 <rathnakumari55555@gmail.com>
2019-09-05 17:39:11 +0530

    changes

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Login from './pages/login';
+import Register from './pages/register';
+import ForgotPassword from './pages/forgotpassword';
+import ResetPassword from './pages/resetpassword';
+import Dashboard from './pages/dashboard';
+// import Search from './';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route path='/login' component={Login}></Route>
+                    <Route path='/' exact component={Login}></Route>
+                    <Route path='/register' component={Register}></Route>
+                    <Route path='/forgotPassword' component={ForgotPassword}></Route>
+                    <Route path='/resetPassword/:token' component={ResetPassword}></Route>
+                    <Route path='/dashboard' component={Dashboard}></Route>
+                    {/* <Route path='/search' component={Search}></Route> */}
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default App;
---
+++ client/src/pages/forgotpassword.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import controller from '../controllers/controller';
+//import Card from '@material-ui/core/Card';
+class forgotPassword extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: "",
+            snackBarMessage: " ",
+            openSnackBar: " "
+        }
+    }
+    handleEmailChange = (event) => {
+        var email = event.target.value;
+        this.setState({
+            email: email
+        })
+    }
+    handleResetpassword = () => {
+        if (this.state.email === "") {
+            this.setState({
+                openSnackBar: true,
+                snackBarMessage: 'email is empty'
+            });
+        }
+        else {
+            controller.forgotPassword(this.state.email)
+            this.props.history.push('/resetPassword')
+        }
+        
+    }
+    handleSnackClose = () => {
+        try {
+            this.setState({
+                openSnackBar: false
+            })
+        } catch (err) {
+            console.log("error at handleSnackClose in login");
+        }
+    }
+
+    render() {
+        return (
+
+            <div>
+                <form className="forgotPassword">
+                    <h1>Forgotpassword Page</h1>
+                    <div>
+                        <TextField
+                            id="outlined-email-input"
+                            label="Email"
+                            type="email"
+                            name="email"
+                            autoComplete="email"
+                            margin="normal"
+                            variant="outlined"
+                            onChange={this.handleEmailChange}
+                            value={this.state.email}
+                        />
+                    </div>
+                    <div className="Fbutton">
+                        <Button variant="outlined" color="primary" onClick={this.handleResetpassword}>
+                            Submit
+                          </Button>
+                    </div>
+                    <Snackbar
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'right',
+                        }}
+                        open={this.state.openSnackBar}
+                        autoHideDuration={6000}
+                        onClose={this.handleSnackClose}
+                        variant="error"
+                        ContentProps={{
+                            'aria-describedby': 'message-id',
+                        }}
+                        message={<span id="message-id"> {this.state.snackBarMessage} </span>}
+                        action={[
+                            <div>
+                                <IconButton
+                                    key="close"
+                                    aria-label="Close"
+                                    color="inherit"
+                                    onClick={this.handleSnackClose}>
+                                    <CloseIcon />
+                                </IconButton>
+                            </div>
+                        ]}
+                    />
+                </form>
+            </div>
+        )
+    }
+}
+export default forgotPassword;
